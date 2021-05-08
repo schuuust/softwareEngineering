668ab2ec61ba230c24a9c99fa8ccb8c9b5efe1a3
Schuster <mschust2@uccs.edu>
2021-05-08 02:26:22 +0000

    HTML is on its way...

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'project1/index'
+  get 'home/index'
   devise_for :users
-  root to: 'posts#index'
+  root to: 'home#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/views/project1/index.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "project1/index.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
